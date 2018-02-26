74e2f2d129ce89d69618694fc42d6c96fe4946be
Zhicheng Wang <zcwang@thoughtworks.com>
2018-02-26 22:24:57 +0800

    feat: use dynamic app id which allocated by the main loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root-app2',
+  // tslint:disable:component-selector
+  selector: 'app-root-__AppLoaderDynamicId__',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   count = 100;
+
   decrease(): void {
-    this.count --;
+    this.count--;
   }
 }
